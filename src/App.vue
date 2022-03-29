69bfc0d26701a1f7fe4a3b157b7184a19437d09a
Roshan <roshanbudhathoki02@gmail.com>
2022-03-30 00:56:44 +0545

    added circle and cube with rotation

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,6 @@ export default {
   name: 'App',
   components: {
     MainContent
-  },
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,28 +2,73 @@
     <div ref="sceneContainer"></div>
 </template>
 <script>
-import { Scene, PerspectiveCamera, BoxGeometry, MeshBasicMaterial, Mesh, WebGLRenderer } from 'three';
+import {
+    Scene,
+    PerspectiveCamera,
+    BoxGeometry,
+    Color,
+    MeshBasicMaterial,
+    Mesh,
+    WebGLRenderer,
+    LineBasicMaterial,
+    BufferGeometry,
+    Vector3,
+    CircleGeometry,
+    Group,
+    Line,
+} from 'three';
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls';
+
 const scene = new Scene();
 const camera = new PerspectiveCamera(
     75, window.innerWidth/window.innerHeight, 0.1, 500
 );
 const renderer = new WebGLRenderer()
-const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({
-    color: 0x00ff00
+
+const geometryCube = new BoxGeometry();
+const materialCube = new MeshBasicMaterial({
+    color: 0xff0000
 });
-const cube = new Mesh(geometry, material);
+
+const material = new MeshBasicMaterial({
+    color: 0xa5a5a5
+});
+
+const materialLine = new LineBasicMaterial({
+    color: 0xFF0000
+});
+const points = [];
+points.push(new Vector3(-10, 0, 0));
+points.push(new Vector3(0, 10, 0));
+points.push(new Vector3(10, 0, 0));
+
+const geometryLine = new BufferGeometry().setFromPoints(points);
+
+const geometry = new CircleGeometry( 2, 100/* , 2, 1.2 * Math.PI */ );
+
+const cube = new Mesh(geometryCube, materialCube);
+const line = new Line(geometryLine, materialLine);
+const circle = new Mesh(geometry, material);
+
+const controls = new OrbitControls( camera, renderer.domElement );
 export default {
     name: 'MainContent',
     data() {
         return {
+
         };
     },
     created() {
-        scene.add(cube);
-        camera.position.z = 5;
-
         renderer.setSize(window.innerWidth, window.innerHeight);
+        camera.position.set(0, 0, 5);
+        controls.update();
+        scene.background = new Color('#000');
+
+        const group = new Group();
+        group.add(cube);
+        group.add(circle);
+        group.add(line);
+        scene.add(group);
     },
     mounted() {
         this.$refs.sceneContainer.appendChild(renderer.domElement);
@@ -32,7 +77,10 @@ export default {
     methods: {
         animate() {
             requestAnimationFrame(this.animate);
+            controls.update();
 
+            circle.rotation.x += 0.01;
+            circle.rotation.y += 0.01;
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
 
